21e059b77f9771cefe20b54d09801bed7f5b0b43
qasim memmedov <qasim_memmedov_94@mail.ru>
2020-03-05 12:21:25 +0400

    Create

---
+++ Prize/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Prize.data;
+using Prize.Models;
+
+namespace Prize.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        private readonly ElPrizeContext _context;
+
+        public HomeController(ElPrizeContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+             
+            int UserId = int.Parse(User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.PrimarySid).Value);
+            ViewBag.cash = _context.Users.Where(c => c.Id == UserId).First().Cash;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Prize/Controllers/WalletController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Prize.data;
+
+namespace Prize.Controllers
+{
+    public class WalletController : Controller
+    {
+        private readonly ElPrizeContext _context;
+
+        public WalletController(ElPrizeContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Valyuta()
+        {
+            int UserId = int.Parse(User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.PrimarySid).Value);
+            ViewBag.cash = _context.Users.Where(c => c.Id == UserId).First().Cash;
+            return View();
+        }
+        public IActionResult Udush()
+        {
+            int UserId = int.Parse(User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.PrimarySid).Value);
+            ViewBag.cash = _context.Users.Where(c => c.Id == UserId).First().Cash;
+            return View();
+        }
+    }
+}
